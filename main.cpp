916b523d170d87bb09e39d4d704d14256d8eb7a9
andrinur13 <andrinur13@gmail.com>
2020-04-01 20:01:10 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,24 +9,48 @@ public:
     //pertambahan
     void pertambahan()
     {
+        cout << "Pertambahan" << endl;
         cout << "Masukkan Nilai X  : ";
         cin >> x;
         cout << "Masukkan Nilai Y  : ";
         cin >> y;
         cout << "Hasil Pertambahan : " << x + y << endl;
-    }
+    };
 
     //pengurangan
     void pengurangan()
     {
+        cout << "Pengurangan" << endl;
         cout << "Masukkan Nilai X  : ";
         cin >> x;
         cout << "Masukkan Nilai Y  : ";
         cin >> y;
         cout << " Hasil Pengurangan : " << x - y << endl;
-    }
+    };
 
     // pembagian
+    void pembagian()
+    {
+        cout << "PEMBAGIAN" << endl;
+        cout << "==========" << endl;
+        cout << " X : ";
+        cin >> x;
+        cout << " Y : ";
+        cin >> y;
+
+        cout << " Hasil : " << x / (float)y;
+    };
+
+    // perkalian
+    void perkalian()
+    {
+        cout << "Perkalian" << endl;
+        cout << "Masukkan Nilai X  : ";
+        cin >> x;
+        cout << "Masukkan Nilai Y  : ";
+        cin >> y;
+        cout << " Hasil Perkalian : " << x * y << endl;
+    }
 };
 
 main()
