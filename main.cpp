7f7d98a41b10df63a4e6916d8f60e868e6acd8aa
AnfieldGank14 <62476501+AnfieldGank14@users.noreply.github.com>
2020-04-01 11:34:40 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,21 +6,29 @@ class Kalkulator
 public:
     int x, y;
 
-    void pengurangan()
+    void pertambahan()
     {
         cout << "Masukkan Nilai X  : ";
         cin >> x;
         cout << "Masukkan Nilai Y  : ";
         cin >> y;
-        cout << "Hasil Pengurangan: " << x - y << endl;
+        cout << "Hasil Pertambahan : " << x + y << endl;
     }
 
+    // pengurangan
+    void pengurangan(){
+        cout << "Masukkan Nilai X  : ";
+        cin >> x;
+        cout << "Masukkan Nilai Y  : ";
+        cin >> y;
+        cout<<" Hasil Pengurangan : "<< x - y <<endl;
+
+    // pembagian
 };
 
 main()
 {
     Kalkulator kalk;
-
     // nanti tinggal running aja methodnya
     // contoh kalk.pertambahan();
 }
