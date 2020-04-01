76ba067f241f40a34fffe186413c49108c60707c
andrinur13 <andrinur13@gmail.com>
2020-04-01 20:04:07 +0700

    update main

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ public:
         cout << " Y : ";
         cin >> y;
 
-        cout << " Hasil : " << x / (float)y;
+        cout << " Hasil : " << x / (float)y << endl;
     };
 
     // perkalian
@@ -49,13 +49,63 @@ public:
         cin >> x;
         cout << "Masukkan Nilai Y  : ";
         cin >> y;
-        cout << " Hasil Perkalian : " << x * y << endl;
+        cout << "Hasil Perkalian : " << x * y << endl;
     }
 };
 
 main()
 {
+awal:
     Kalkulator kalk;
     // nanti tinggal running aja methodnya
     // contoh kalk.pertambahan();
+    cout << "=========== Kalkulator Sederhana" << endl;
+    cout << "Pilihan Operasi" << endl;
+    cout << "1. Pertambahan" << endl;
+    cout << "2. Pengurangan" << endl;
+    cout << "3. Perkalian" << endl;
+    cout << "4. Pembagian" << endl;
+pilihan:
+    cout << "Pilih : ";
+    int pil;
+    cin >> pil;
+
+    switch (pil)
+    {
+    case 1:
+        kalk.pertambahan();
+        break;
+
+    case 2:
+        kalk.pengurangan();
+        break;
+
+    case 3:
+        kalk.perkalian();
+        break;
+
+    case 4:
+        kalk.pembagian();
+        break;
+
+    default:
+        cout << "Salah Input!";
+        goto pilihan;
+    }
+
+    char y;
+
+    cout << "Apakah Anda ingin mengulang lagi [y/n] ? ";
+    cin >> y;
+
+    if (y == 'y')
+    {
+        goto awal;
+    }
+    else
+    {
+
+        cout << endl;
+        cout << "Terimakasih" << endl;
+    }
 }
